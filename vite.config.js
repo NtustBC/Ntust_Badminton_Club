bea32a9f9a9c485c4ff7a2b3b26663361f8e19f8
CharlesOuO <linchihjia@gmail.com>
2026-06-16 23:23:42 +0800

    v0.1.16

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         classSignup: fileURLToPath(new URL('./class-signup.html', import.meta.url)),
         notices: fileURLToPath(new URL('./notices.html', import.meta.url)),
         faq: fileURLToPath(new URL('./faq.html', import.meta.url)),
+        members: fileURLToPath(new URL('./members.html', import.meta.url)),
         privacy: fileURLToPath(new URL('./privacy.html', import.meta.url)),
       },
     },
